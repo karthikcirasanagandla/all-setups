ba5ccb4e5bdb613c458d76c8710f63a88a7622e9
karthikcirasanagandla <158122392+karthikcirasanagandla@users.noreply.github.com>
2024-02-05 21:27:25 +0530

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -4,4 +4,4 @@ systemctl status httpd
 cd /var/www/html
 git clone https://github.com/CleverProgrammers/pwj-netflix-clone.git
 mv pwj-netflix-clone/* .
-tail -100f /var/log/httpd/access_log
+tail -10f /var/log/httpd/access_log
